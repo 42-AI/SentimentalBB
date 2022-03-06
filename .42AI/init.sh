607908e694f2352d1a094c74021443f86d254c9d
ezalos <ezalos@gmail.com>
2022-03-06 16:03:23 +0100

    fix(init.sh): using python from virtualenv

--- .42AI/init.sh
+++ .42AI/init.sh
@@ -3,28 +3,32 @@
 RED="\e[1;31m"
 END="\e[0m"
 
-echo $RED "INIT: " $END "Copy of hook commit..."
-mkdir -p .git/hooks/
-cp .42AI/pre-commit.git .git/hooks/pre-commit
+echo $1/.envrc
 
+source $1/.envrc
+python --version
 
-echo $RED "INIT: " $END "Testing your python version..."
-python .42AI/test_environment.py
+# echo $RED "INIT: " $END "Copy of hook commit..."
+# mkdir -p .git/hooks/
+# cp .42AI/pre-commit.git .git/hooks/pre-commit
 
-echo $RED "INIT: " $END "Creating data directories..."
-mkdir -p data/raw
-mkdir -p data/processed
-mkdir -p data/external
-mkdir -p data/interim
+# echo $RED "INIT: " $END "Testing your python version..."
+# python .42AI/test_environment.py
 
-echo $RED "INIT: " $END "Upgrading pip..."
-pip install --upgrade pip
+# echo $RED "INIT: " $END "Creating data directories..."
+# mkdir -p data/raw
+# mkdir -p data/processed
+# mkdir -p data/external
+# mkdir -p data/interim
 
-echo $RED "INIT: " $END "Installing python dependancies..."
-pip install -r requirements.txt
+# echo $RED "INIT: " $END "Upgrading pip..."
+# pip install --upgrade pip
 
-# echo $RED "INIT: " $END "Downloading hello world dataset..."
-# sh scripts/download_dataset.sh
+# echo $RED "INIT: " $END "Installing python dependancies..."
+# pip install -r requirements.txt
 
-echo $RED "INIT: " $END "Adding .envrc ..."
-echo "source venv/bin/activate" > .envrc
+# # echo $RED "INIT: " $END "Downloading hello world dataset..."
+# # sh scripts/download_dataset.sh
+
+# echo $RED "INIT: " $END "Adding .envrc ..."
+# echo "source venv/bin/activate" > .envrc
