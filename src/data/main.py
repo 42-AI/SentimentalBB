bb3689afc52de7b9a952a2d8688e61b33a0e1d4c
Guillaume SALLE <gusalle@student.42.fr>
2022-03-06 18:04:44 +0100

    feat(data): 1st download of macron dataset -> currently broken import

--- src/data/main.py
+++ src/data/main.py
@@ -1,5 +1,9 @@
-from src.data.make_dataset import make_dataset_aclImdb
+from src.data.make_dataset.aclImdb import make_dataset_aclImdb
+from src.data.make_dataset.macron import make_dataset_macron
 
 
-def data_main():
-    make_dataset_aclImdb()
+def data_main(dataset_download: str):
+    if dataset_download == "aclImdb":
+        make_dataset_aclImdb()
+    elif dataset_download == "macron":
+        make_dataset_macron()
---
+++ src/data/make_dataset/macron.py
@@ -0,0 +1,32 @@
+import searchtweets
+from searchtweets import ResultStream, gen_request_parameters, load_credentials
+from searchtweets import collect_results
+import pandas as pd
+
+user = "macron"
+MAX_TWEET = 100
+SAVE_PATH = f"data/raw/twitter/{user}/"
+
+
+def make_dataset_macron():
+    # Usage can be found here:
+    #   https://github.com/twitterdev/search-tweets-python/blob/46386e0316b79096b5c5369abbc4f361b8153622/searchtweets/credentials.py#L114
+    search_args = load_credentials(env_overwrite=False)
+    query = gen_request_parameters(
+        "Emmanuel Macron", results_per_call=100, granularity=None)
+    print(query)
+    tweets = collect_results(query, max_tweets=MAX_TWEET,
+                             result_stream_args=search_args)
+
+    def func(tweets, tweet_nb): return tweets[0]['data'][tweet_nb]['text']
+
+    tweet_list = [func(tweets, i) for i in range(MAX_TWEET)]
+
+    pre_df = {
+        "text": tweet_list,
+    }
+
+    df = pd.DataFrame.from_dict(pre_df)
+    csv_path = f"{SAVE_PATH}first_list.csv"
+    os.makedirs(os.path.dirname(csv_path), exist_ok=True)
+    df.to_csv(csv_path)
