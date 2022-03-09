32495b33af5fae52bdd61e3ed81c12f13065b05c
Guillaume SALLE <gusalle@student.42.fr>
2022-03-09 21:49:10 +0100

    no error if yaml file present

--- src/data/make_dataset/macron.py
+++ src/data/make_dataset/macron.py
@@ -9,11 +9,12 @@ MAX_TWEET = 10
 SAVE_PATH = f"data/raw/twitter/{user}/"
 KEYS_YAML_PATH = f".twitter_keys.yaml"
 
+
 def make_dataset_macron():
     # Usage can be found here:
     #   https://github.com/twitterdev/search-tweets-python/blob/v2/searchtweets/credentials.py
     search_args = load_credentials(filename=KEYS_YAML_PATH,
-    yaml_key="search_tweets_v2_recent", env_overwrite=False)
+                                   yaml_key="search_tweets_v2_recent", env_overwrite=False)
     query = gen_request_parameters(
         "Emmanuel Macron", results_per_call=MAX_TWEET, granularity=None)
     print(query)
