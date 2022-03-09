2f6c6a10060621a38677ac65e8dad602b226d830
Guillaume SALLE <gusalle@student.42.fr>
2022-03-09 21:36:46 +0100

    minor change to macron.py and add credentials yaml to gitignore

--- src/data/make_dataset/macron.py
+++ src/data/make_dataset/macron.py
@@ -7,14 +7,13 @@ import os
 user = "macron"
 MAX_TWEET = 10
 SAVE_PATH = f"data/raw/twitter/{user}/"
-KEYS_PATH_YAM = f".twitter_keys.yaml"
-
+KEYS_YAML_PATH = f".twitter_keys.yaml"
 
 def make_dataset_macron():
     # Usage can be found here:
     #   https://github.com/twitterdev/search-tweets-python/blob/v2/searchtweets/credentials.py
-    search_args = load_credentials(filename=KEYS_PATH_YAM,
-    yaml_key="search_tweet_v2_recent", env_overwrite=False)
+    search_args = load_credentials(filename=KEYS_YAML_PATH,
+    yaml_key="search_tweets_v2_recent", env_overwrite=False)
     query = gen_request_parameters(
         "Emmanuel Macron", results_per_call=MAX_TWEET, granularity=None)
     print(query)
