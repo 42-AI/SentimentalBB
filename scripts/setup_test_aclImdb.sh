479f5331dfcb199dbbf260a4397c61386bb04f23
Guillaume SALLE <gusalle@student.42.fr>
2022-03-11 16:45:33 +0100

    feat(setup tests): do not create train/test dataset if they exists

--- scripts/setup_test_aclImdb.sh
+++ scripts/setup_test_aclImdb.sh
@@ -1,3 +1,15 @@
-sh scripts/download_dataset.sh
-python -m src data --download aclImdb
+bash scripts/download_dataset.sh
+
+PATH_DATASET_TRAIN="data/processed/aclImdb/aclImdb_test.csv"
+
+if [ -f "$PATH_DATASET_TRAIN" ]
+then
+echo "Dataset already exist, lets train !"
+else
+    echo "Dataset do not exist yet lets make dataset !"
+    python -m src data --download aclImdb
+fi
+
+
+
 python -m src models --train data/processed/aclImdb/aclImdb_train.csv --test data/processed/aclImdb/aclImdb_test.csv --model "naive-bayes"
