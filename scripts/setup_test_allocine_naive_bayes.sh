c5be791a00fff93e70956f396ee6894be0f64f87
Archibald THIRION <athirion@student.42.fr>
2022-03-13 16:40:26 +0100

    cicd(allocine): fix wrong name variable

--- scripts/setup_test_allocine_naive_bayes.sh
+++ scripts/setup_test_allocine_naive_bayes.sh
@@ -1,25 +1,26 @@
+#! /bin/bash -
 
 PATH_DATASET_TRAIN="data/processed/allocine/train.csv"
 PATH_DATASET_TEST="data/processed/allocine/test.csv"
 
 if [ -f "$PATH_DATASET_TRAIN" ]
 then
-echo "Dataset already exist, lets train !"
+echo "Dataset train already exist"
 else
     echo "Dataset do not exist yet lets make dataset !"
     poetry run python -m src data --download allocine --split train
 fi
 
 
-if [ -f "$PATH_DATASET_TRAIN" ]
+if [ -f "$PATH_DATASET_TEST" ]
 then
-echo "Dataset already exist, lets train !"
+echo "Dataset test already exist"
 else
     echo "Dataset do not exist yet lets make dataset !"
     poetry run python -m src data --download allocine --split test
 fi
 
 
-poetry run python -m src models --model naive-bayes --train data/processed/allocine/train.csv --test data/processed/allocine/test.csv
+poetry run python -m src models --model naive-bayes --train $PATH_DATASET_TRAIN --test $PATH_DATASET_TEST
 
 poetry run pytest
