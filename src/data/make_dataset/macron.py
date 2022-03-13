e6865d9cab7c6f9c567cb0be84982d1414d777fc
Guillaume SALLE <gusalle@student.42.fr>
2022-03-13 15:46:23 +0100

    test modifications on macron.py

--- src/data/make_dataset/macron.py
+++ src/data/make_dataset/macron.py
@@ -16,20 +16,33 @@ def make_dataset_macron():
     search_args = load_credentials(filename=KEYS_YAML_PATH,
                                    yaml_key="search_tweets_v2_recent", env_overwrite=False)
     query = gen_request_parameters(
-        "Emmanuel Macron", results_per_call=MAX_TWEET, granularity=None)
+                                    "Emmanuel Macron",
+                                    results_per_call=MAX_TWEET,
+                                    granularity=None,
+                                    tweet_fields="id,created_at,text")
+    #[attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,
+    #lang,non_public_metrics,organic_metrics,possibly_sensitive,promoted_metrics,public_metrics,referenced_tweets,
+    #reply_settings,source,text,withheld]
     print(query)
-    tweets = collect_results(query, max_tweets=MAX_TWEET,
-                             result_stream_args=search_args)
+    tweets = collect_results(query,
+                            max_tweets=MAX_TWEET,
+                            result_stream_args=search_args)
+    print(tweets)
 
-    def func(tweets, tweet_nb): return tweets[0]['data'][tweet_nb]['text']
+    #def func(tweets, tweet_nb): return tweets[0]['data'][tweet_nb]['text']
+    def func(tweets, field, tweet_nb): return tweets[0]['data'][tweet_nb][field]
 
-    tweet_list = [func(tweets, i) for i in range(MAX_TWEET)]
+    #tweet_list = [func(tweets, 'text', i) for i in range(MAX_TWEET)]
+    def tweet_list(field): return [func(tweets, field, i) for i in range(MAX_TWEET)]
 
     pre_df = {
-        "text": tweet_list,
+        field: tweet_list(field) for field in tweets[0]['data'][0]
     }
+        #"text": tweet_list,
+        #"created_at": [func(tweets, field, i) for i in range(MAX_TWEET)]
 
     df = pd.DataFrame.from_dict(pre_df)
+    print(df)
     csv_path = f"{SAVE_PATH}first_list.csv"
-    os.makedirs(os.path.dirname(csv_path), exist_ok=True)
-    df.to_csv(csv_path)
+    #os.makedirs(os.path.dirname(csv_path), exist_ok=True)
+    #df.to_csv(csv_path)
