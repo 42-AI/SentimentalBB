633cf4b29e9fed91663c4d90e2458514c26b58d1
Archibald THIRION <athirion@student.42.fr>
2022-03-13 16:20:21 +0100

    test(allocine): don't create dataset if already there

--- scripts/setup_test_allocine_naive_bayes.sh
+++ scripts/setup_test_allocine_naive_bayes.sh
@@ -1,4 +1,25 @@
-poetry run python -m src data --download allocine --split train
-poetry run python -m src data --download allocine --split test
+
+PATH_DATASET_TRAIN="data/processed/allocine/train.csv"
+PATH_DATASET_TEST="data/processed/allocine/test.csv"
+
+if [ -f "$PATH_DATASET_TRAIN" ]
+then
+echo "Dataset already exist, lets train !"
+else
+    echo "Dataset do not exist yet lets make dataset !"
+    poetry run python -m src data --download allocine --split train
+fi
+
+
+if [ -f "$PATH_DATASET_TRAIN" ]
+then
+echo "Dataset already exist, lets train !"
+else
+    echo "Dataset do not exist yet lets make dataset !"
+    poetry run python -m src data --download allocine --split test
+fi
+
+
 poetry run python -m src models --model naive-bayes --train data/processed/allocine/train.csv --test data/processed/allocine/test.csv
+
 poetry run pytest
