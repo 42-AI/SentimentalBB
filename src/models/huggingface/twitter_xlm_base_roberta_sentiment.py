c180c9159b769ca9d7328f3054b4f5afbef5b5b3
Guillaume SALLE <guillaume.salle@gmail.com>
2022-03-20 22:06:20 +0100

    fix double file huggingface and notebook twitter

--- src/models/huggingface/twitter_xlm_base_roberta_sentiment.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline
-
-model_path = "cardiffnlp/twitter-xlm-roberta-base-sentiment"
-pipeline = pipeline("sentiment-analysis",
-                    model=model_path, tokenizer=model_path)
