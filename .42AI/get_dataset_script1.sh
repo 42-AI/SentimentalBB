9cb5dc2fc75070180a31b753aa0ca76be0e1f088
mdavid <mdavid@student.42.fr>
2022-03-24 18:27:11 +0100

    data (dataset generation): script to download tweets of all the candidats in 4 hours range in a day

---
+++ .42AI/get_dataset_script1.sh
@@ -0,0 +1,30 @@
+#! /bin/bash -
+
+# Color related variables
+COLOR_CYAN="\e[1;36m"
+COLOR_PURPLE="\e[1;35m"
+COLOR_RESET="\e[0m"
+
+# request forging Twitter API related variables
+CANDIDATS=("Pecresse" "Zemmour" "Dupont-Aignan" "Melenchon" "Le Pen" "Lassalle" "Hidalgo" "Macron" "Jadot" "Roussel" "Arthaud" "Poutou")
+DD="23"
+DATE_STARTS=("2022-03-$DD 8:00" "2022-03-$DD 12:00" "2022-03-$DD 16:00" "2022-03-$DD 20:00")
+DATE_ENDS=("2022-03-$DD 12:00" "2022-03-$DD 16:00" "2022-03-$DD 20:00" "2022-03-$DD 23:59")
+
+
+# Print array values in  lines
+echo "Print every element in new line"
+for candidat in "${CANDIDATS[@]}"; do
+    for i in "${!DATE_STARTS[@]}"; do
+        echo -e $COLOR_CYAN "downloading..." "$candidat" "${DATE_STARTS[i]}" "${DATE_ENDS[i]}" $COLOR_RESET
+        poetry run python -m src data --download twitter --mention "$candidat" --start_time "${DATE_STARTS[i]}" --end_time "${DATE_ENDS[i]}"
+    done
+    echo -e $COLOR_PURPLE "... sleeping 30 seconds to avoid limit rate ..." $COLOR_RESET
+    sleep 30
+done
+
+# poetry run dvc add data.dvc
+# poetry run dvc push
+# git add data.dvc
+# git commit -m $1
+# git push
