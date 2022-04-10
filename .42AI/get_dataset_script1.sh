490ecd046704d1fcce1174c24ee6928a65f68616
Guillaume SALLE <guillaume.salle@gmail.com>
2022-04-10 12:58:34 +0200

    script fixed and tweets for 04-04 downloaded

--- .42AI/get_dataset_script1.sh
+++ .42AI/get_dataset_script1.sh
@@ -7,9 +7,9 @@ COLOR_RESET="\e[0m"
 
 # request forging Twitter API related variables
 CANDIDATS=("Pecresse" "Zemmour" "Dupont-Aignan" "Melenchon" "Le Pen" "Lassalle" "Hidalgo" "Macron" "Jadot" "Roussel" "Arthaud" "Poutou")
-DD="23"
-DATE_STARTS=("2022-03-$DD 8:00" "2022-03-$DD 12:00" "2022-03-$DD 16:00" "2022-03-$DD 20:00")
-DATE_ENDS=("2022-03-$DD 12:00" "2022-03-$DD 16:00" "2022-03-$DD 20:00" "2022-03-$DD 23:59")
+DD="04-04"
+DATE_STARTS=("2022-$DD 0:00" "2022-$DD 4:00" "2022-$DD 8:00" "2022-$DD 12:00" "2022-$DD 16:00" "2022-$DD 20:00")
+DATE_ENDS=("2022-$DD 4:00" "2022-$DD 8:00" "2022-$DD 12:00" "2022-$DD 16:00" "2022-$DD 20:00" "2022-$DD 23:59")
 
 
 # Print array values in  lines
@@ -17,10 +17,10 @@ echo "Print every element in new line"
 for candidat in "${CANDIDATS[@]}"; do
     for i in "${!DATE_STARTS[@]}"; do
         echo -e $COLOR_CYAN "downloading..." "$candidat" "${DATE_STARTS[i]}" "${DATE_ENDS[i]}" $COLOR_RESET
-        poetry run python -m src data --download twitter --mention "$candidat" --start_time "${DATE_STARTS[i]}" --end_time "${DATE_ENDS[i]}"
+        poetry run python -m src data --task download --data twitter --candidate "$candidat" --start_time "${DATE_STARTS[i]}" --end_time "${DATE_ENDS[i]}"
     done
-    echo -e $COLOR_PURPLE "... sleeping 30 seconds to avoid limit rate ..." $COLOR_RESET
-    sleep 30
+    echo -e $COLOR_PURPLE "... sleeping 60 seconds to avoid limit rate ..." $COLOR_RESET
+    sleep 60
 done
 
 # poetry run dvc add data.dvc
