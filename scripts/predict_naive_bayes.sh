f103ed22886a6e1511b7fc79dc9795fcd3174f1b
Guillaume SALLE <guillaume.salle@gmail.com>
2022-04-11 19:00:51 +0200

    visuals generated for 0410

--- scripts/predict_naive_bayes.sh
+++ scripts/predict_naive_bayes.sh
@@ -6,12 +6,12 @@ COLOR_PURPLE="\e[1;35m"
 COLOR_RESET="\e[0m"
 
 DD="0405"
-wheights="068_Naive_Bayes_04-11_18:15.z"
+wheights="068_Naive_Bayes_04-10_21:50.z"
 
-echo "Predict on data in data/processed/twitter/predict/" "$DD"
+echo "Predict on data in data/processed/twitter/predict/$DD..."
 for filename in data/processed/twitter/predict/${DD}/*.csv; do
 	echo -e $COLOR_CYAN "processing..." "$filename" $COLOR_RESET
-		poetry run python -m src models --task predict --model  naive-bayes --weights_in models/${wheights} --dataset_type predict --flat_y --in_csv ${filename}
+	poetry run python -m src models --task predict --model  naive-bayes --weights_in models/${wheights} --dataset_type predict --flat_y --in_csv ${filename}
 done
 
 # poetry run dvc add data
