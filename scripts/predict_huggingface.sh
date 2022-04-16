0b278e761e5e2d9e4298bdaebf47666349f58ea8
Guillaume SALLE <guillaume.salle@gmail.com>
2022-04-16 21:33:01 +0200

    add model_name in visualization funcion

--- scripts/predict_huggingface.sh
+++ scripts/predict_huggingface.sh
@@ -7,6 +7,7 @@ COLOR_RESET="\e[0m"
 
 DD="0410"
 D2="04-10"
+candidates=("macron" "lepen")
 
 if [ ! -d data/processed/twitter/predict/$DD ]; then
 	echo -e $COLOR_CYAN "Data of ${DD} is not processed. Processing..." $COLOR_RESET
@@ -14,9 +15,11 @@ if [ ! -d data/processed/twitter/predict/$DD ]; then
 fi
 
 echo "Predict on data in data/processed/twitter/predict/$DD..."
-for filename in data/processed/twitter/predict/${DD}/*.csv; do
-	echo -e $COLOR_CYAN "processing..." "$filename" $COLOR_RESET
-	poetry run python -m src models --task predict --model  huggingface --dataset_type predict --flat_y --in_csv ${filename}
+for candidate in "${candidates[@]}"; do
+	for filename in data/processed/twitter/predict/${DD}/${candidate}*.csv; do
+		echo -e $COLOR_CYAN "processing..." "$filename" $COLOR_RESET
+		poetry run python -m src models --task predict --model  huggingface --dataset_type predict --flat_y --in_csv ${filename}
+	done
 done
 
 # poetry run dvc add data
